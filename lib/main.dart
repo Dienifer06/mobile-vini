f0fb62803faaa89e302dd151dddf8be2dc718510
Dienifer06 <dieniferoliveira0310@gmail.com>
2026-02-18 11:24:07 -0300

    passo 2 flutter

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,40 @@ class TelaInicial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home:Scaffold(
+        appBar:AppBar(title:Text("App1")),
+        body:Column(
+          spacing:20,
+          mainAxisAlignment:MainAxisAlignment.center, //eixo principal
+          crossAxisAlignment:CrossAxisAlignment.start,
+          
+          children:[
+            Container(width:100,height:100,color:Colors.pink),
+            Container(width: 100,height:100, color: Colors.purple),
+            Container(width: 100,height: 100,color: Colors.cyanAccent),
+            Row(spacing:20,
+              children: [
+                Container(width:100,height:100, color: Colors.tealAccent),
+                Container(width:100,height:100,color:Colors.blueAccent),
+                Container(width:100,height:100,color:Colors.black87)
+              ],
+            ),
+            SizedBox(height: 50),
+            Stack(
+              alignment:AlignmentGeometry.center,
+              children:[
+                Container(width:250,height:200,color:Colors.black),
+                Container(width:200,height:150,color:Colors.amberAccent),
+                Container(width:150,height:100, color:Colors.blueGrey),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
 
